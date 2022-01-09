523f83365e62edf413831cb42acdc212d94089d2
Bhitthawat Chindawat <bhitthawat.ch@hotmail.com>
2022-01-09 16:07:38 +0700

    make it better

--- app.js
+++ app.js
@@ -7,132 +7,177 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const request = require("request");
 const { isNull } = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 
 const homeStartingContent =
   "สวัสดีครับพี่ๆจากทุกบริษัท ยินดีต้อนรับสู่โปรเจคเว็บ Blog ของผมที่จัดทำขึ้นนะครับ พี่ๆสามารถกดปุ่ม COMPOSE (มุมขวาบน) เพื่อโพสต์ข้อความในเว็บ Blog และสามารถกดปุ่ม Delete เพื่อลบโพสต์ได้ครับ ขอให้สนุกกับโปรเจคของผมครับ ❤";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Our little Secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(
-  "mongodb+srv://france2541:077821083@cluster0.rd9j1.mongodb.net/blogDB?retryWrites=true&w=majority"
+  "mongodb+srv://france2541:077821083@cluster0.rd9j1.mongodb.net/blogv2?retryWrites=true&w=majority"
 );
 
-const blog = mongoose.model("blog", { title: String, content: String });
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
-app.set("view engine", "ejs");
+userSchema.plugin(passportLocalMongoose);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const User = new mongoose.model("User", userSchema);
+
+const blog = new mongoose.model("blog", {
+  title: String,
+  content: String,
+  authorID: String,
+  authorUsername: String,
+});
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
-  request(
-    {
-      method: "POST",
-      uri: "https://notify-api.line.me/api/notify",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      auth: {
-        bearer: "tCTzhJ91NOYoKA0C9x2jsW4HdYU5EFOdxWob6vKeR79",
-      },
-      form: {
-        message: "\nขณะนี้มีผู้คนเข้ามาชมเว็บไซต์ของคุณ",
-      },
-    },
-    (err, httpResponse, body) => {
+  if (req.isAuthenticated()) {
+    res.redirect("/blog");
+  } else {
+    res.render("home");
+  }
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
       if (err) {
         console.log(err);
+        res.redirect("/register");
       } else {
-        console.log(body);
-      }
-    }
-  );
-  blog.find({}, function (err, foundItems) {
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: foundItems,
-    });
-  });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
-});
-
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-
-  const newPost = new blog(post);
-
-  newPost.save(function (err) {
-    res.redirect("/");
-  });
-
-  request(
-    {
-      method: "POST",
-      uri: "https://notify-api.line.me/api/notify",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      auth: {
-        bearer: "tCTzhJ91NOYoKA0C9x2jsW4HdYU5EFOdxWob6vKeR79",
-      },
-      form: {
-        message:
-          "\nชื่อโพสต์: " + post.title + "\n" + "เนื้อหา: " + post.content,
-      },
-    },
-    (err, httpResponse, body) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(body);
-      }
-    }
-  );
-});
-
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  blog.find({}, function (err, foundItems) {
-    foundItems.forEach(function (post) {
-      const storedTitle = _.lowerCase(post.title);
-
-      if (storedTitle === requestedTitle) {
-        res.render("post", {
-          title: post.title,
-          content: post.content,
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/blog");
         });
       }
-    });
+    }
+  );
+});
+
+app.get("/login", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect("/blog");
+  } else {
+    res.render("login");
+  }
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/blog");
+      });
+    }
   });
 });
 
-app.post("/", function (req, res) {
-  const postDelete = req.body.postDelete;
-  console.log(postDelete);
+app.get("/blog", function (req, res) {
+  if (req.isAuthenticated()) {
+    blog.find({}, function (err, foundpost) {
+      res.render("blog", {
+        startingContent: homeStartingContent,
+        posts: foundpost,
+        username: req.user.username,
+      });
+    });
 
-  blog.deleteOne({ title: postDelete }, function (err) {
-    if (!err) {
+    app.get("/compose", function (req, res) {
+      res.render("compose");
+    });
+
+    app.post("/compose", function (req, res) {
+      const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody,
+        authorID: req.user.id,
+        authorUsername: req.user.username,
+      };
+
+      const newPost = new blog(post);
+
+      newPost.save(function () {
+        res.redirect("/blog");
+      });
+    });
+
+    app.post("/", function (req, res) {
+      var deleteTitle = req.body.postDelete;
+
+      blog.findOne({ title: deleteTitle }, function (err, foundpost) {
+        console.log(foundpost);
+        console.log(req.user.id);
+        if (foundpost.authorID === req.user.id) {
+          blog.deleteOne({ title: deleteTitle }, function () {
+            res.redirect("/blog");
+          });
+        } else {
+          res.redirect("/blog");
+        }
+      });
+    });
+
+    app.get("/posts/:postName", function (req, res) {
+      const requestedTitle = _.lowerCase(req.params.postName);
+
+      blog.find({}, function (err, foundItems) {
+        foundItems.forEach(function (post) {
+          const storedTitle = _.lowerCase(post.title);
+
+          if (storedTitle === requestedTitle) {
+            res.render("post", {
+              title: post.title,
+              content: post.content,
+            });
+          }
+        });
+      });
+    });
+
+    app.get("/logout", function (req, res) {
+      req.logout();
       res.redirect("/");
-    }
-  });
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 let port = process.env.PORT;
@@ -140,5 +185,3 @@ if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port);
-
-// app.listen(3000);
