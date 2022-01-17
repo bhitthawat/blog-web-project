d295edc0f67489c32af9435f31b21258594a5e1d
bhitthawat <90171445+bhitthawat@users.noreply.github.com>
2022-01-17 11:44:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Our little Secret",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
   })
